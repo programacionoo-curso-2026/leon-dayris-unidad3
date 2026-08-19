206d2709d3837efc433797a37984b07aed3d9d6b
LeonDay02 <anahisleonvera2007@gmail.com>
2026-08-18 19:35:51 -0500

    Deber5: Archivo docente.go creado, contiene el struct de docente

---
+++ deber5-docente_dao/model/docente.go
@@ -0,0 +1,11 @@
+// model/docente.go
+package model
+
+type Docente struct {
+	ID              string
+	Nombre          string
+	Email           string
+	Departamento    string
+	Cargo           string
+	AniosAntiguedad int
+}
